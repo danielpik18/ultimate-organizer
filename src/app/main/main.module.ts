5f1a5efc14a2c7e439a05a677d7c573743438d93
Daniel <danielmomusic@gmail.com>
2020-01-02 16:52:55 -0500

    Add floating menu I

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { FloatingMenuComponent } from './floating-menu/floating-menu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, FloatingMenuComponent],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomeComponent
   ]
 })
 export class MainModule { }
